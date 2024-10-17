bd49d19765e324a618126875b6376cdf99ac5a8f
David Muriithi <muriithid05@gmail.com>
2024-10-17 10:12:38 +0300

    modified:   lazy-lock.json 	modified:   lua/plugins/init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -115,16 +115,54 @@ return {
       rocks = { "magick" },
     },
   },
-	{
-	 	"3rd/image.nvim",
- 		dependencies = { "luarocks.nvim" },
- 		config = function()
- 		require("image").setup({
- 		backend = "ueberzug",  -- ueberzug,
- 		max_height_window_percentage = 50,
- 		hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.svg" },
- 		})
-	end
-	}
+  {
+    "3rd/image.nvim",
+    dependencies = { "luarocks.nvim" },
+    config = function()
+      require("image").setup({
+        backend = "ueberzug", -- ueberzug,
+        max_height_window_percentage = 50,
+        hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.svg" },
+      })
+    end
+  },
+
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+  }
 
 }
